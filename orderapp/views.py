bcede266219e972a4570b0e2d6ed3f432abbb718
WAF <kolanlos510@gmail.com>
2019-08-23 11:00:20 +0300

    views, urls

--- orderapp/views.py
+++ orderapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import Order
+from .serializers import OrderSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all().order_by('-cost')
+    serializer_class = OrderSerializer
